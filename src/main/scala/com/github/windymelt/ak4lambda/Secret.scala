bbb65dd85fef86f23b5a2ecec438304b8edd5b17
windymelt <windymelt@3qe.us>
2024-02-01 23:55:01 +0900

    use IO

--- src/main/scala/com/github/windymelt/ak4lambda/Secret.scala
+++ src/main/scala/com/github/windymelt/ak4lambda/Secret.scala
@@ -3,11 +3,14 @@ package com.github.windymelt.ak4lambda
 import com.amazonaws.secretsmanager.caching.SecretCache
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder
 import com.amazonaws.services.secretsmanager.model.UpdateSecretRequest
+import cats.effect.IO
 
 object Secret:
   private val cache = new SecretCache()
-  def currentToken(secretArn: String): String = cache.getSecretString(secretArn)
-  def updateCurrentToken(secretArn: String, token: String): Unit =
+  def currentToken(secretArn: String): IO[String] = IO:
+    cache.getSecretString(secretArn)
+
+  def updateCurrentToken(secretArn: String, token: String): IO[Unit] = IO:
     val req =
       new UpdateSecretRequest().withSecretId(secretArn).withSecretString(token)
     AWSSecretsManagerClientBuilder.defaultClient().updateSecret(req)
