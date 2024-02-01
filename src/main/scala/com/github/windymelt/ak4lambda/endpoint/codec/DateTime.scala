2b0b5c95e63775449c18e04e0b6eee1e45b07834
windymelt <windymelt@3qe.us>
2024-02-02 00:29:57 +0900

    implement datetime decoder

--- src/main/scala/com/github/windymelt/ak4lambda/endpoint/codec/DateTime.scala
+++ src/main/scala/com/github/windymelt/ak4lambda/endpoint/codec/DateTime.scala
@@ -1,12 +1,10 @@
 package com.github.windymelt.ak4lambda.endpoint.codec
 
-import com.github.nscala_time.time.Implicits._
-import io.circe.Decoder
-import io.circe.Encoder
-import io.circe.HCursor
-import io.circe.Json
+import com.github.nscala_time.time.Implicits.*
+import io.circe.{Decoder, DecodingFailure, Encoder, HCursor, Json}
 import org.joda.time.DateTimeZone
-import org.joda.time.format.DateTimeFormatter
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import scala.util.control.Exception.allCatch
 
 object DateTime {
   import org.joda.time.DateTime
@@ -22,7 +20,15 @@ object DateTime {
 
   implicit val decodeDateTime: Decoder[DateTime] = new Decoder[DateTime] {
     final def apply(s: HCursor): Decoder.Result[DateTime] = {
-      ???
+      val fmt = DateTimeFormat.forPattern("""yyyy/MM/dd HH:mm:ss""")
+      for
+        dtString <- s.focus
+          .flatMap(_.asString)
+          .toRight(DecodingFailure("Could not parse as string", s.history))
+        dt <- (allCatch either {
+          org.joda.time.DateTime.parse(dtString, fmt)
+        }).left.map(e => DecodingFailure(e.getMessage, s.history))
+      yield dt
     }
   }
 }
